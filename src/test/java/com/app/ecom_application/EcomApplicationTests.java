a2ab3960c66b028d1f8f6175646c9e90d73ae168
fede-quinteros <142957085+fede-quinteros@users.noreply.github.com>
2026-03-12 16:36:08 -0300

    add docker-compose

--- src/test/java/com/app/ecom_application/EcomApplicationTests.java
+++ src/test/java/com/app/ecom_application/EcomApplicationTests.java
@@ -1,7 +1,5 @@
 package com.app.ecom_application;
 
-import com.app.ecom_application.models.User;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
